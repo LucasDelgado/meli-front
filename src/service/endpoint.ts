30b70be7fcadea14bb82fa751c761d1a5e555809
Delgado Lucas (Diseño - lanacion digital) <ldelgado@lanacion.com.ar>
2022-08-08 23:59:49 -0300

    add api url env

--- src/service/endpoint.ts
+++ src/service/endpoint.ts
@@ -1,8 +1,9 @@
-const API_URL: string | undefined = "http://localhost:5000/api/";
+const API_URL: string | undefined =
+  process.env.API_URL || "http://localhost:5000/api/";
 
 const endpoint = {
   items: {
-    findAll: (search: string) => `${API_URL}/items?q=${search}`,
+    findAll: (search: string) => `${API_URL}items?q=${search}`,
     findOne: (id: string) => `${API_URL}items/${id}`,
   },
 };
